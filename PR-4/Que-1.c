ac16669bdbc7f9e85d09211b7934f10e6522207a
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-21 12:51:59 +0530

    Implement nested loops for number pattern
    Added nested loops to print a pattern of numbers.

--- PR-4/Que-1.c
+++ PR-4/Que-1.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  
+
+  int i,j;
+
+  //? Starting the loop
+
+  for (i = 1;i <= 5;i++){
+
+    //? Starting second loop
+
+    for (j = 0;j < i;j++){
+      printf("%d ", 41 + j);
+    }
+
+    printf("\n");
+  }  
   return 0;
 }
