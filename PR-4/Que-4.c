42deeaa4057773c7b2c59c4a946d97ff721594f2
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-22 20:39:35 +0530

    Update comments for clarity

--- PR-4/Que-4.c
+++ PR-4/Que-4.c
@@ -2,27 +2,27 @@
 
 int main() {
 
-    // Declaring the variables
+    //? Declaring the variables
     int i, j;
 
-    // Starting the main loop
+    //? Starting the main loop
     for (i = 5; i >= 1; i--) {
 
-        // Starting the loop for space
+        //! Starting the loop for space
         for (j = i; j < 5; j++) {
             printf("  ");
         }
 
-        // Starting the loop for 1s or 0s printing
+        //! Starting the loop for 1s or 0s printing
         for (j = 1; j <= i; j++) {
 
-            // Ternary operator for 1s and 0s in code
+            //? Ternary operator for 1s and 0s in code
             (j % 2 == 1) ? printf("1 ") : printf("0 ");
         }
 
-        // To print in next row
+        //! To print in next row
         printf("\n");
     }
 
-    return 0; // Program executed successfully
+    return 0; //? Program executed successfully
 }
