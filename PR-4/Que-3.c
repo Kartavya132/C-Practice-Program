074b39e3b688f0c71dac4e9260af211635c19b49
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-22 20:38:13 +0530

    Update comments for clarity

--- PR-4/Que-3.c
+++ PR-4/Que-3.c
@@ -2,23 +2,23 @@
 
 int main() {
 
-    // Declaring the variables
+    //? Declaring the variables
     int i, j;
 
-    // Starting the main loop
+    //? Starting the main loop
     for (i = 5; i >= 1; i--) {
 
-        // Starting the loop for space in the output
+        //! Starting the loop for space in the output
         for (j = i - 1; j >= 1; j--) {
             printf("  ");
         }
 
-        // Starting the loop for integer output
+        //! Starting the loop for integer output
         for (j = i; j <= 5; j++) {
             printf("%d ", j);
         }
 
-        // Output to next line
+        //! Output to next line
         printf("\n");
     }
 
