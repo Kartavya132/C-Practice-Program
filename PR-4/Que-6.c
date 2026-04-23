4c77b0ad0bb946b42dcdd34d4bf9c58d95b93b63
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-23 11:47:30 +0530

    Update comments for clarity

--- PR-4/Que-6.c
+++ PR-4/Que-6.c
@@ -4,28 +4,28 @@ int main()
 {
     int i, j;
 
-    // Starting the main loop
+    //? Starting the main loop
     for (i = 1; i <= 5; i++)
     {
-        // Starting the loop for inclination
+        //! Starting the loop for inclination
         for (j = 1; j <= i; j++)
         {
             printf("%d ", j);
         }
 
-        // Starting the loop for space
+        //! Starting the loop for space
         for (j = 5 - i; j >= 1; j--)
         {
             printf("    ");  
         }
 
-        // Starting the loop for declination
+        //! Starting the loop for declination
         for (j = i; j >= 1; j--)
         {
             printf("%d ", j);
         }
 
-        // Move to next line after each row
+        //? Printing next loop
         printf("\n");
     }
 
