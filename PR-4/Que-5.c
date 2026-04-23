f448cf449823b80f6339f459052966b51b133629
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-23 11:33:15 +0530

    Add C program to print a pattern of numbers

---
+++ PR-4/Que-5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+
+    //? Declaring the variables
+    int i, j, k, n = 5;
+
+    //? Starting the main loop
+    for (i = n; i >= 1; i--) {
+
+        //! Starting the loop for space
+        for (j = 1; j < i; j++) {
+            printf("   ");
+        }
+
+        //! Starting the loop for inclination
+        for (k = i; k <= n; k++) {
+            printf("%d ", k);
+        }
+
+        //! Starting the loop for declination
+        for (k = n - 1; k >= i; k--) {
+            printf("%d ", k);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
