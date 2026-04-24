a8f10193b9f10b0891163b384a3c03279b257ad7
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-24 11:23:07 +0530

    Implement program to find negative elements in array
    This program takes an array input from the user and prints all negative elements found in the array. If no negative elements are present, it informs the user accordingly.

---
+++ PR-5/Que-1.c
@@ -0,0 +1,39 @@
+//TODO: All Negative elements in array
+
+#include <stdio.h>
+
+#define MAX 100
+
+int main() {
+
+    // Declaring the elements
+    int a[MAX];
+    int i, n, found = 0;
+
+    // Taking the input for lenght of array
+    printf("Enter the limit: ");
+    scanf("%d", &n);
+
+    // Input array elements
+    printf("\nEnter the elements of the array:\n");
+    for (i = 0; i < n; i++) {
+        printf("Element %d: ", i + 1);
+        scanf("%d", &a[i]);
+    }
+
+    // Printing all negative elements in arrays
+    printf("\nNegative elements in the array are:\n");
+    for (i = 0; i < n; i++) {
+        if (a[i] < 0) {
+            printf("%d ", a[i]);
+            found = 1;
+        }
+    }
+
+    // If no negative element exists
+    if (found == 0) {
+        printf("No negative elements found.");
+    }
+
+    return 0;
+}
