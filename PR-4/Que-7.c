617eb49b5fdde2f77b00a749c446ec4297d0270b
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-24 11:05:55 +0530

    Enhance code readability with additional comments
    Added comments to clarify code functionality.

--- PR-4/Que-7.c
+++ PR-4/Que-7.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main() {
+    // Declaring the variable
     int rows = 5;
     int cols = 5;
 
-    // Outer loop controls rows
+    // Starting the main loop
     for (int i = 0; i < rows; i++) {
 
-        // Inner loop controls columns
+        // Starting the inside loop
         for (int j = 0; j < cols; j++) {
 
+            // Condition for printing the 
             if (i == 0 || i == 2 || j == 0 || (j == cols - 1 && i == 1)) {
                 printf("* ");
             } else {
-                printf("  ");   // Print spaces for empty part
+                printf("  ");  
             }
         }
 
