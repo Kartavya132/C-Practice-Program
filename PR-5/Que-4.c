4a9a927c4515be7b3933fbf15dc64f0b7a221627
Kartavya Patel <kartavyapatel630@gmail.com>
2026-04-29 11:33:28 +0530

    Refactor comments for clarity in Que-4.c

--- PR-5/Que-4.c
+++ PR-5/Que-4.c
@@ -4,26 +4,27 @@
 
 int main()
 {
-  int a[MAX][MAX]; // 2D array to store matrix
-  int n, m;        // Number of rows and columns
+  // Declaring the variables and arrays
+  int a[MAX][MAX]; 
+  int n, m;        
   int i, j;
   int row_n, column_n;
   int sum = 0;
 
-  // Ask user for number of rows and columns
+  // Taking input of no. of rows and columns
   printf("Enter number of rows: ");
   scanf("%d", &n);
 
   printf("Enter number of columns: ");
   scanf("%d", &m);
 
-  // Input elements into the matrix
+  // Taking input of elements of the arrays
   printf("\nEnter matrix elements:\n");
   for (i = 0; i < n; i++)
   {
     for (j = 0; j < m; j++)
     {
-      scanf("%d", &a[i][j]); // Read each element
+      scanf("%d", &a[i][j]); 
     }
   }
 
@@ -33,30 +34,30 @@ int main()
   {
     for (j = 0; j < m; j++)
     {
-      printf("%d ", a[i][j]); // Print elements
+      printf("%d ", a[i][j]); 
     }
     printf("\n");
   }
 
-  // Find sum of a specific row
+  // Taking input for finding the sum of the row
   printf("\nEnter row number to find sum: ");
   scanf("%d", &row_n);
 
-  sum = 0; // Reset sum
+  sum = 0; 
   for (j = 0; j < m; j++)
   {
-    sum += a[row_n - 1][j]; // Add elements of that row
+    sum += a[row_n - 1][j]; 
   }
   printf("Sum of row %d = %d\n", row_n, sum);
 
-  // Find sum of a specific column
+  // Taking input for finding the sum of the column
   printf("\nEnter column number to find sum: ");
   scanf("%d", &column_n);
 
-  sum = 0; // Reset sum again
+  sum = 0;
   for (i = 0; i < n; i++)
   {
-    sum += a[i][column_n - 1]; // Add elements of that column
+    sum += a[i][column_n - 1];
   }
   printf("Sum of column %d = %d\n", column_n, sum);
 
