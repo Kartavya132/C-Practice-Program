4faebdcd1d7151d7bcdac6b524d0d21ae6b6903e
Kartavya Patel <kartavyapatel630@gmail.com>
2026-05-04 12:02:56 +0530

    Add C program to count character frequency in a string

---
+++ PR-6/Que-2.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    int i, j, len, count;
+    
+    printf("Enter any string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    // Starting the main loop
+    for(i = 0; i < len; i++) {
+        count = 1;
+
+        // Condition for repeatation
+        if(str[i] == '\0')
+            continue;
+
+        // Loop for frequency
+        for(j = i + 1; j < len; j++) {
+            if(str[i] == str[j]) {
+                count++;
+                str[j] = '\0';
+            }
+        }
+
+        // Printing the frequency
+        printf("%c => %d\n", str[i], count);
+    }
+
+    return 0;
+}
