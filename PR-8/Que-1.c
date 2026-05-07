1167d631070a27eda930f55cf7058a39239b6e43
Kartavya Patel <kartavyapatel630@gmail.com>
2026-05-07 16:07:28 +0530

    Implement string length calculation using pointers
    Added functionality to read a string and calculate its length using pointers.

--- PR-8/Que-1.c
+++ PR-8/Que-1.c
@@ -1,4 +1,27 @@
-#include<stdio.h>
-int main(){
-  return 0;
+#include <stdio.h>
+
+int main() 
+{
+    char str[100];
+    char *ptr;
+    int length = 0;
+
+    // Read the string from user
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Store address of string in pointer
+    ptr = str;
+
+    // Count characters using pointer
+    while (*ptr != '\0')
+    {
+        length++;
+        ptr++;
+    }
+
+    // Print the length of string
+    printf("Length of string = %d", length);
+
+    return 0;
 }
